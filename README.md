9180607bde43d5af3aacb895d273b00ff82a41cf
Gwen <gwendall.esnault@gmail.com>
2015-03-16 14:58:48 +0100

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+Meteoric Connection Status
+==========================
+
+Installation
+------------
+
+``` sh
+meteor add gwendall:bindings
+```
+
+How it works
+----------
+
+Just drop the {{> ionConnectionStatus}} template in your {{#ionBody}} block.
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: "gwendall:meteoric-connection-status",
+  summary: "Connection status in Meteoric",
+  git: "https://github.com/gwendall/meteoric-connection-status.git",
+  version: "0.1.0"
+});
+
+Package.onUse(function (api, where) {
+
+  api.use([
+    "jquery@1.11.3",
+    "templating@1.0.11",
+  ], "client");
+
+  api.addFiles([
+    "lib/status.html",
+    "lib/status.css",
+    "lib/status.js"
+  ], "client");
+
+});
