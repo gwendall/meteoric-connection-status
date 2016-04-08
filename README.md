41fd754e713333f651caf276e959c0f32a3a9d08
Brent Abrahams <brent_abrahams@yahoo.com>
2016-04-08 10:33:25 +0800

    Syncing local and remote repos

---
+++ README.md
@@ -0,0 +1,14 @@
+Meteoric Connection Status
+==========================
+
+Installation
+------------
+
+``` sh
+meteor add gwendall:meteoric-connection-status
+```
+
+How it works
+----------
+
+Just drop the {{> ionConnectionStatus}} template in your {{#ionBody}} block.
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: "gwendall:meteoric-connection-status",
+  summary: "Connection status in Meteoric",
+  git: "https://github.com/gwendall/meteoric-connection-status.git",
+  version: "0.2.0"
+});
+
+Package.onUse(function (api, where) {
+
+  api.use([
+    "jquery",
+    "templating",
+  ], "client");
+
+  api.addFiles([
+    "lib/status.html",
+    "lib/status.css",
+    "lib/status.js"
+  ], "client");
+
+});
